36f6f194f5f3cac7529a97511103e1c974815241
Shpend Aliu <shpendaliu@69gmail.com>
2023-02-04 23:42:33 +0100

    Put the outputs in a file

--- DailyQoute.sh
+++ DailyQoute.sh
@@ -5,7 +5,11 @@ read -r -p "Enter your input [r for random | c for show categories]: " answer
 
 if [ "$answer" == "r" ]; then
 
-    curl -s "https://quotes.rest/qod" | jq '.contents.quotes[0].quote'
+    curl -s "https://quotes.rest/qod" | jq '.contents.quotes[0].quote' | (
+        echo -e "\n"
+        echo "You generated this on: $(date)"
+        cat
+    ) | tee -a qoutes.txt
 
 elif
 
@@ -18,56 +22,88 @@ then
 
     if [ "$catInput" == "love" ]; then
 
-        curl -s "https://quotes.rest/qod/love" | jq '.contents.quotes[0].quote'
+        curl -s "https://quotes.rest/qod/love" | jq '.contents.quotes[0].quote' | (
+            echo -e "\n"
+            echo "You generated this on: $(date)"
+            cat
+        ) | tee -a qoutes.txt
 
     elif
 
         [ "$catInput" == "art" ]
     then
 
-        curl -s "https://quotes.rest/qod/art" | jq '.contents.quotes[0].quote'
+        curl -s "https://quotes.rest/qod/art" | jq '.contents.quotes[0].quote' | (
+            echo -e "\n"
+            echo "You generated this on: $(date)"
+            cat
+        ) | tee -a qoutes.txt
 
     elif
 
         [ "$catInput" == "funny" ]
     then
 
-        curl -s "https://quotes.rest/qod/funny" | jq '.contents.quotes[0].quote'
+        curl -s "https://quotes.rest/qod/funny" | jq '.contents.quotes[0].quote' | (
+            echo -e "\n"
+            echo "You generated this on: $(date)"
+            cat
+        ) | tee -a qoutes.txt
 
     elif
 
         [ "$catInput" == "inspire" ]
     then
 
-        curl -s "https://quotes.rest/qod/inspire" | jq '.contents.quotes[0].quote'
+        curl -s "https://quotes.rest/qod/inspire" | jq '.contents.quotes[0].quote' | (
+            echo -e "\n"
+            echo "You generated this on: $(date)"
+            cat
+        ) | tee -a qoutes.txt
 
     elif
 
         [ "$catInput" == "management" ]
     then
 
-        curl -s "https://quotes.rest/qod/management" | jq '.contents.quotes[0].quote'
+        curl -s "https://quotes.rest/qod/management" | jq '.contents.quotes[0].quote' | (
+            echo -e "\n"
+            echo "You generated this on: $(date)"
+            cat
+        ) | tee -a qoutes.txt
 
     elif
 
         [ "$catInput" == "sports" ]
     then
 
-        curl -s "https://quotes.rest/qod/sports" | jq '.contents.quotes[0].quote'
+        curl -s "https://quotes.rest/qod/sports" | jq '.contents.quotes[0].quote' | (
+            echo -e "\n"
+            echo "You generated this on: $(date)"
+            cat
+        ) | tee -a qoutes.txt
 
     elif
 
         [ "$catInput" == "students" ]
     then
 
-        curl -s "https://quotes.rest/qod/students" | jq '.contents.quotes[0].quote'
+        curl -s "https://quotes.rest/qod/students" | jq '.contents.quotes[0].quote' | (
+            echo -e "\n"
+            echo "You generated this on: $(date)"
+            cat
+        ) | tee -a qoutes.txt
 
     elif
 
         [ "$catInput" == "life" ]
     then
 
-        curl -s "https://quotes.rest/qod/life" | jq '.contents.quotes[0].quote'
+        curl -s "https://quotes.rest/qod/life" | jq '.contents.quotes[0].quote' | (
+            echo -e "\n"
+            echo "You generated this on: $(date)"
+            cat
+        ) | tee -a qoutes.txt
 
     fi
 
